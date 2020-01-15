e1e34a9db77a8aa953c0a8fc0ac56644c9298b5c
your nameJeff <jeff.wesner@usd.edu>
2020-01-15 13:53:43 -0600

    update
    update

--- scripts/1_script_load_data.R
+++ scripts/1_script_load_data.R
@@ -7,35 +7,44 @@ library(cowplot)
 library(janitor)
 library(repmis)
 
-# Load most recent data from github ---------------------------------------------------
+# Load data ---------------------------------------------------
 data_fish <- readRDS("~/GitHub/Freshwater-Fish-Diet-Database/database/data_fish.rds")
 
 #get column names
 cols <- names(data_fish)
 
-#function to add columns to the new data sheets data_sheet if not already included
-fncols <- function(data, cname) {
-  add <-cname[!cname%in%names(data)]
+
+# Functions ---------------------------------------------------------------
+fish_gather <- function(dt) {
+  dt <- dt %>% gather(prey_taxon, measurement, -"fish_genus_species":-"data_sorted_by") %>% 
+      clean_names()}
+
+#function to add columns to the new data sheets if not already included
+fish_cols <- function(data, cname) {
+  add <- cname[!cname%in%names(data)]
   
   if(length(add)!=0) data[add] <- NA
   data
 }
 
 # make list of filenames in your data folder
-filename <- list.files("./database/data_to_add")
+filename <- list.files("./data_editedRaw/2020-01-13")
 
 # empty object to put data into
 data.list <- NULL
 
 # for loop
 for(name in seq_along(filename)){
-  data.list[[name]] <- read.csv(paste("database/data_to_add/", filename[name], sep = ""))
+  data.list[[name]] <- read.csv(paste("data_editedRaw/2020-01-13/", filename[name], sep = ""),fileEncoding="UTF-8-BOM")
 }
 
 # below this you could combine to one call _____________________________
-new_data <- lapply(data.list, FUN = fishTidy)
+new_data <- lapply(data.list, FUN = fish_gather)
 
-gather(prey_taxon, measurement, -"site_name":-"notes")
 
-setdiff(centr_cols, cols)
+
+
+
+
+test <- rbind.fill(data_fish, community_adite_1997_tbl3)
 
