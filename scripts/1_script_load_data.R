69bdd4b0d89be031d0db5a77b0b17e140406db3a
your nameJeff <jeff.wesner@usd.edu>
2020-01-15 13:56:09 -0600

    moved folders to database folder

--- scripts/1_script_load_data.R
+++ scripts/1_script_load_data.R
@@ -17,7 +17,8 @@ cols <- names(data_fish)
 # Functions ---------------------------------------------------------------
 fish_gather <- function(dt) {
   dt <- dt %>% gather(prey_taxon, measurement, -"fish_genus_species":-"data_sorted_by") %>% 
-      clean_names()}
+      clean_names() %>% 
+    mutate(measurement = as.numeric(measurement))}
 
 #function to add columns to the new data sheets if not already included
 fish_cols <- function(data, cname) {
@@ -28,23 +29,26 @@ fish_cols <- function(data, cname) {
 }
 
 # make list of filenames in your data folder
-filename <- list.files("./data_editedRaw/2020-01-13")
+filename <- list.files("./data_editedRaw/2020-01-14")
 
 # empty object to put data into
 data.list <- NULL
 
-# for loop
+# add files to list
 for(name in seq_along(filename)){
-  data.list[[name]] <- read.csv(paste("data_editedRaw/2020-01-13/", filename[name], sep = ""),fileEncoding="UTF-8-BOM")
+  data.list[[name]] <- read.csv(paste("data_editedRaw/2020-01-14/", filename[name], sep = ""),fileEncoding="UTF-8-BOM")
 }
 
-# below this you could combine to one call _____________________________
-new_data <- lapply(data.list, FUN = fish_gather)
+# make a new data frame + add columns to it that are in the master data frame
+new_data <- bind_rows(unclass(lapply(data.list, FUN = fish_gather))) %>% 
+  fish_cols(cols)
+
+#append data to master data frame
+data_fish2020115 <- rbind(new_data, data_fish)
+
+#save it with date of creation stamp
+saveRDS(data_fish2020115, file = "data_fish2020115.rds")
 
 
 
-
-
-
-test <- rbind.fill(data_fish, community_adite_1997_tbl3)
 
