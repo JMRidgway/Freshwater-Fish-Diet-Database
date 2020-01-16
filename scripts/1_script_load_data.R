ff1355cba624d4c2a4214af56ba14070907b8fee
your nameJeff <jeff.wesner@usd.edu>
2020-01-16 10:52:33 -0600

    update

--- scripts/1_script_load_data.R
+++ scripts/1_script_load_data.R
@@ -8,16 +8,16 @@ library(janitor)
 library(repmis)
 
 # Load data ---------------------------------------------------
-data_fish <- readRDS("~/GitHub/Freshwater-Fish-Diet-Database/database/data_fish.rds")
-
+data_fish_current <- readRDS("~/GitHub/Freshwater-Fish-Diet-Database/database/data_fish.rds")
 #get column names
-cols <- names(data_fish)
+cols <- names(data_fish_current)
 
 
 # Functions ---------------------------------------------------------------
 fish_gather <- function(dt) {
-  dt <- dt %>% gather(prey_taxon, measurement, -"fish_genus_species":-"data_sorted_by") %>% 
-      clean_names() %>% 
+  dt <- dt  %>% 
+    clean_names() %>% 
+    gather(prey_taxon, measurement, -"fish_genus_species":-"data_sorted_by") %>% 
     mutate(measurement = as.numeric(measurement))}
 
 #function to add columns to the new data sheets if not already included
@@ -29,14 +29,14 @@ fish_cols <- function(data, cname) {
 }
 
 # make list of filenames in your data folder
-filename <- list.files("./data_editedRaw/2020-01-14")
+filename <- list.files("database/data_to_add/2020-01-15")
 
 # empty object to put data into
 data.list <- NULL
 
 # add files to list
 for(name in seq_along(filename)){
-  data.list[[name]] <- read.csv(paste("data_editedRaw/2020-01-14/", filename[name], sep = ""),fileEncoding="UTF-8-BOM")
+  data.list[[name]] <- read.csv(paste("database/data_to_add/2020-01-15/", filename[name], sep = ""),fileEncoding="UTF-8-BOM")
 }
 
 # make a new data frame + add columns to it that are in the master data frame
@@ -44,10 +44,10 @@ new_data <- bind_rows(unclass(lapply(data.list, FUN = fish_gather))) %>%
   fish_cols(cols)
 
 #append data to master data frame
-data_fish2020115 <- rbind(new_data, data_fish)
+data_fish <- rbind(new_data, data_fish_current)
 
-#save it with date of creation stamp
-saveRDS(data_fish2020115, file = "data_fish2020115.rds")
+#save updated version
+saveRDS(data_fish, file = "database/data_fish.rds")
 
 
 
