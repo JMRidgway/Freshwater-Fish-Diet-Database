c68a732fda887140b102178257690d8e88c5b71e
your nameJeff <jeff.wesner@usd.edu>
2020-01-16 14:18:59 -0600

    update folders

--- scripts/1_script_load_data.R
+++ scripts/1_script_load_data.R
@@ -8,7 +8,8 @@ library(janitor)
 library(repmis)
 
 # Load data ---------------------------------------------------
-data_fish_current <- readRDS("~/GitHub/Freshwater-Fish-Diet-Database/database/data_fish.rds")
+data_fish_current <- readRDS("~/GitHub/Freshwater-Fish-Diet-Database/database/data_fish.rds") %>% 
+  mutate_all(funs("as.character"))
 #get column names
 cols <- names(data_fish_current)
 
@@ -17,38 +18,33 @@ cols <- names(data_fish_current)
 fish_gather <- function(dt) {
   dt <- dt  %>% 
     clean_names() %>% 
-    gather(prey_taxon, measurement, -"fish_genus_species":-"data_sorted_by") %>% 
-    mutate(measurement = as.numeric(measurement))}
+    gather(prey_taxon, measurement, -"site_name":-"notes") %>% 
+    mutate_each(funs("as.character"))}
 
-#function to add columns to the new data sheets if not already included
-fish_cols <- function(data, cname) {
-  add <- cname[!cname%in%names(data)]
-  
-  if(length(add)!=0) data[add] <- NA
-  data
-}
-
-# make list of filenames in your data folder
-filename <- list.files("database/data_to_add/2020-01-15")
+# make list of filenames from the data in your folder
+filename <- list.files("database/data_already_added/2019-12-14")
 
 # empty object to put data into
 data.list <- NULL
 
 # add files to list
 for(name in seq_along(filename)){
-  data.list[[name]] <- read.csv(paste("database/data_to_add/2020-01-15/", filename[name], sep = ""),fileEncoding="UTF-8-BOM")
+  data.list[[name]] <- read.csv(paste("database/data_already_added/2019-12-14/", filename[name], sep = ""))
 }
 
 # make a new data frame + add columns to it that are in the master data frame
-new_data <- bind_rows(unclass(lapply(data.list, FUN = fish_gather))) %>% 
-  fish_cols(cols)
+
+new_data <- bind_rows(unclass(lapply(data.list, FUN = fish_gather))) 
 
 #append data to master data frame
-data_fish <- rbind(new_data, data_fish_current)
+data_fish <- bind_rows(new_data, data_fish_current) %>% 
+  mutate(type_of_fish = case_when(is.na(type_of_fish) ~ fish_genus_species,
+                                  TRUE ~ type_of_fish))
 
 #save updated version
 saveRDS(data_fish, file = "database/data_fish.rds")
 
+distinct(data_fish, author)
 
 
 
