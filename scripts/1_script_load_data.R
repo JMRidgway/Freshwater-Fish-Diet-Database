5f003e454d81eecdc18a99f7ac56a33840ac1f60
your nameJeff <jeff.wesner@usd.edu>
2020-01-19 11:05:01 -0600

    update

--- scripts/1_script_load_data.R
+++ scripts/1_script_load_data.R
@@ -16,7 +16,7 @@ data_fish_current <- readRDS("~/GitHub/Freshwater-Fish-Diet-Database/database/da
   mutate_all(funs("as.character")) 
 
 #folder to import from - name of folder in the working directory that contains extracted csvs to add
-folder <- "2020-01-18"
+folder <- "data_already_added/2020-01-18"
 
 # Functions ---------------------------------------------------------------
 #fish_gather cleans and gathers the imported data and adds life stage information
@@ -24,7 +24,7 @@ fish_gather <- function(dt) {
   dt <- dt  %>% 
     clean_names() %>% 
     remove_empty("rows") %>% 
-    rename_at(1, ~"site_name") %>% 
+    rename_at(1, ~"site_name") %>%
     gather(prey_taxon, measurement, -"site_name":-"notes") %>% 
     mutate_each(funs("as.character")) %>% 
     mutate(prey_stage = case_when(grepl("arva", prey_taxon) ~"larvae",
@@ -33,6 +33,7 @@ fish_gather <- function(dt) {
                                   grepl("upae", prey_taxon) ~ "pupae",
                                   grepl("pupa", prey_taxon) ~ "pupae",
                                   grepl("Pupa", prey_taxon) ~ "pupae",
+                                  grepl("immatur", prey_taxon) ~ "larvae",
                                   TRUE ~ "unknown"))}
 
 # make list of filenames to import ***double check that folder is assigned correctly ***
@@ -71,11 +72,11 @@ lat_lon <- combine_data %>%
   mutate_all(funs('as.character'))
 
 #add lat/lon to data
-new_data <- merge(lat_lon, combine_data)
+new_data <- combine_data %>% left_join(lat_lon)
 
 #append new data to master data frame and extract prey stage information
 data_fish <- bind_rows(new_data, data_fish_current) 
 
 #save updated version of master data frame
 saveRDS(data_fish, file = "database/data_fish.rds")
-
+#now take this data to open refine to clean
