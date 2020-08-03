9087a9c8a75ac2668d50ea3a1f513a1f3b332415
Jeff Wesner <jeff.wesner@usd.edu>
2020-08-03 13:34:09 -0500

    update measurement units and measurement type

--- scripts/script_column_checks.R
+++ scripts/script_column_checks.R
@@ -1,14 +1,14 @@
 str(data_fish)
 
 
-unique(data_fish$sample_size) #good, but need to clean up
+unique(data_fish$sample_size) #good, but need to clean up (sent email to Jacob on 8/1/2020 - what to do with "under")
 unique(data_fish$start_date) #good
 unique(data_fish$end_date) #good
-unique(sort(data_fish$measurement_type)) #check
-unique(sort(data_fish$measurement_typeunits)) #check
-unique(sort(data_fish$measurement_units)) #check
-unique(sort(data_fish$measurement_units_total_percent_other)) #check
-unique(sort(data_fish$measurement_units_total_percent_other)) #check
+unique(sort(data_fish$measurement_type)) #good
+unique(sort(data_fish$measurement_typeunits)) #good
+unique(sort(data_fish$measurement_units)) #good
+# unique(sort(data_fish$measurement_units_total_percent_other)) #removed column
+# unique(sort(data_fish$measurement_units_total_percent_other)) #removed column
 unique(sort(data_fish$year)) #good
 unique(sort(data_fish$habitat)) #good, but need to clean up
 unique(sort(data_fish$table_figure)) #good, but need to clean up
