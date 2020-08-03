101cb738bfcf75d693a952ad6e45b6ee8c507d48
Jeff Wesner <jeff.wesner@usd.edu>
2020-08-03 17:18:40 -0500

    Update script_column_checks.R

--- scripts/script_column_checks.R
+++ scripts/script_column_checks.R
@@ -10,22 +10,16 @@ unique(sort(data_fish$measurement_units)) #good
 # unique(sort(data_fish$measurement_units_total_percent_other)) #removed column
 # unique(sort(data_fish$measurement_units_total_percent_other)) #removed column
 unique(sort(data_fish$year)) #good
-unique(sort(data_fish$habitat)) #good (confirmed that all are freshwater habitats - 08/03/2020)
+unique(sort(data_fish$habitat)) #good (confirmed that all are freshwater habitats/removed three papers that were marine - 08/03/2020)
 unique(sort(data_fish$habitat_general)) #good
 unique(sort(data_fish$table_figure)) #good
 unique(sort(data_fish$measurement)) #check (sent email to Jacob on 8/3/2020 to fix typo's)
-unique(sort(data_fish$author)) #good, but need to clean up
-unique(sort(data_fish$prey_stage)) #good, but need to clean up
-unique(sort(data_fish$sampling_interval)) #good, but need to clean up
+unique(sort(data_fish$author)) #good
+unique(sort(data_fish$prey_stage)) #good
+unique(sort(data_fish$sampling_interval)) #good
 unique(sort(data_fish$lat)) #good
 unique(sort(data_fish$lon)) #good
 
 
-test %>% filter(grepl(">", author)) %>% View()
-test <- test %>% mutate(author = str_replace(author, "<c9>", "E"),
-                             author_year_table = str_replace(author_year_table, "<c9>", "E"),
-                             author_year_tbl = str_replace(author_year_tbl, "<c9>", "E"))
 
-
-
-
+                                  
