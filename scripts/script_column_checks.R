2ab6e4cbb27a1c80b34b10c38192a3ebe1f78eb2
Jeff Wesner <jeff.wesner@usd.edu>
2020-08-03 14:13:23 -0500

    cleaned up table_figure formats

--- scripts/script_column_checks.R
+++ scripts/script_column_checks.R
@@ -10,9 +10,9 @@ unique(sort(data_fish$measurement_units)) #good
 # unique(sort(data_fish$measurement_units_total_percent_other)) #removed column
 # unique(sort(data_fish$measurement_units_total_percent_other)) #removed column
 unique(sort(data_fish$year)) #good
-unique(sort(data_fish$habitat)) #good, but check bays and lagoons. Are these freshwater?
+unique(sort(data_fish$habitat)) #good (confirmed that all are freshwater habitats - 08/03/2020)
 unique(sort(data_fish$habitat_general)) #good
-unique(sort(data_fish$table_figure)) #good, but need to clean up
+unique(sort(data_fish$table_figure)) #good
 unique(sort(data_fish$measurement)) #check
 unique(sort(data_fish$author)) #good, but need to clean up
 unique(sort(data_fish$prey_stage)) #good, but need to clean up
@@ -21,3 +21,7 @@ unique(sort(data_fish$lat)) #good
 unique(sort(data_fish$lon)) #good
 
 
+data_fish <- data_fish %>% mutate(table_figure = str_replace_all(table_figure, "tbl1...8", "tbl1"))
+
+data_fish %>% filter(str_detect(table_figure, "tbl1...8")) %>% View()
+data_fish %>% filter(grepl("Dutton", author)) %>% distinct(table_figure, .keep_all = T) %>% View()                                                                                   
