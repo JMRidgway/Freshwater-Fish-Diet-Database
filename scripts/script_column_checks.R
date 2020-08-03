8cdb9f190bea298bb753ee92f0a88fc6fae9c706
Jeff Wesner <jeff.wesner@usd.edu>
2020-08-03 17:16:24 -0500

    updated authors and prey stage

--- scripts/script_column_checks.R
+++ scripts/script_column_checks.R
@@ -13,7 +13,7 @@ unique(sort(data_fish$year)) #good
 unique(sort(data_fish$habitat)) #good (confirmed that all are freshwater habitats - 08/03/2020)
 unique(sort(data_fish$habitat_general)) #good
 unique(sort(data_fish$table_figure)) #good
-unique(sort(data_fish$measurement)) #check
+unique(sort(data_fish$measurement)) #check (sent email to Jacob on 8/3/2020 to fix typo's)
 unique(sort(data_fish$author)) #good, but need to clean up
 unique(sort(data_fish$prey_stage)) #good, but need to clean up
 unique(sort(data_fish$sampling_interval)) #good, but need to clean up
@@ -21,14 +21,11 @@ unique(sort(data_fish$lat)) #good
 unique(sort(data_fish$lon)) #good
 
 
-unique(sort(test$measurement)) #check
-test2 <- data_fish %>% filter(is.na(measure_numeric))
+test %>% filter(grepl(">", author)) %>% View()
+test <- test %>% mutate(author = str_replace(author, "<c9>", "E"),
+                             author_year_table = str_replace(author_year_table, "<c9>", "E"),
+                             author_year_tbl = str_replace(author_year_tbl, "<c9>", "E"))
 
-fix_measurement <- test2 %>% distinct(measurement, author, citation) %>% mutate(measurement = tolower(measurement)) %>% arrange(measurement) %>%
-  filter(measurement != "trace") %>% 
-  filter(!grepl("<", measurement)) %>% 
-  filter(!grepl(">", measurement)) %>%
-  mutate(how_to_fix = "") %>%
-  select(measurement, how_to_fix, everything())
 
-write.csv(fix_measurement, file = "database/data_to_add/re_do/fix_measurement.csv", row.names = F)
+
+
