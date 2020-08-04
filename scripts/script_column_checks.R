cf13f381e861f1d1293b6d9ab7ded50da822d162
Jeff Wesner <jeff.wesner@usd.edu>
2020-08-04 09:24:34 -0500

    update

--- scripts/script_column_checks.R
+++ scripts/script_column_checks.R
@@ -2,15 +2,15 @@ str(data_fish)
 
 
 unique(data_fish$sample_size) #good, but need to clean up (sent email to Jacob on 8/1/2020 - what to do with "under")
-unique(data_fish$start_date) #good
-unique(data_fish$end_date) #good
+unique(data_fish$start_date) #good - need common format
+unique(data_fish$end_date) #good- need common format
 unique(sort(data_fish$measurement_type)) #good
 unique(sort(data_fish$measurement_typeunits)) #good
 unique(sort(data_fish$measurement_units)) #good
 # unique(sort(data_fish$measurement_units_total_percent_other)) #removed column
 # unique(sort(data_fish$measurement_units_total_percent_other)) #removed column
 unique(sort(data_fish$year)) #good
-unique(sort(data_fish$habitat)) #good (confirmed that all are freshwater habitats/removed three papers that were marine - 08/03/2020)
+unique(sort(data_fish$habitat)) # check
 unique(sort(data_fish$habitat_general)) #good
 unique(sort(data_fish$table_figure)) #good
 unique(sort(data_fish$measurement)) #check (sent email to Jacob on 8/3/2020 to fix typo's)
@@ -21,5 +21,3 @@ unique(sort(data_fish$lat)) #good
 unique(sort(data_fish$lon)) #good
 
 
-
-                                  
